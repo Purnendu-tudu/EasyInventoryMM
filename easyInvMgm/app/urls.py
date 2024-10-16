4a784336dd5b1e0db223f6dcdee6131029bd9a6f
purnendu-tudu <purnendu2udu@gmail.com>
2024-10-16 11:58:00 +0530

    favicon added in the site

--- easyInvMgm/app/urls.py
+++ easyInvMgm/app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import index , inventory_list, per_producut_view, add_product, delete_product, update_product, dashboard
 
 urlpatterns = [
-    path("",index, name="index"),
+    path("",index, name="indexHome"),
     path("inventorylist/", inventory_list, name="inventoryList"),
     path("product/<int:pk>", per_producut_view, name="product" ),
     path("addProduct/",add_product, name="addProduct"),
